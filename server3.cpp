6e2dadea366431edd3899077e95c2984beabc848
coutino568 <40869460+coutino568@users.noreply.github.com>
2022-05-04 16:47:37 -0600

    Update server3.cpp

--- server3.cpp
+++ server3.cpp
@@ -1,17 +1,14 @@
-//Example code: A simple server side code, which echos back the received message.
-//Handle multiple socket connections with select and fd_set on Linux
 
-//PRueba del ejemplo de geeks for geeks
 #include <stdio.h>
-#include <string.h> //strlen
+#include <string.h> 
 #include <stdlib.h>
 #include <errno.h>
-#include <unistd.h> //close
-#include <arpa/inet.h> //close
+#include <unistd.h> 
+#include <arpa/inet.h> 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
+#include <sys/time.h> 
 #include "json.hpp"
 #include <iomanip>
 #include <iostream>
@@ -137,7 +134,7 @@ int main(int argc , char *argv[])
         perror("bind failed");  
         exit(EXIT_FAILURE);  
     }  
-    printf("Listener on port %d \n", PORT);  
+    
          
     //try to specify maximum of 3 pending connections for the master socket 
     if (listen(master_socket, 3) < 0)  
@@ -146,25 +143,20 @@ int main(int argc , char *argv[])
         exit(EXIT_FAILURE);  
     }  
          
-    //accept the incoming connection 
+
     addrlen = sizeof(address);  
-    puts("ONLINE ...");  
+    puts(" NOW ONLINE ...");  
          
     while(TRUE)  
-    {  
-        //clear the socket set 
+    {   
         FD_ZERO(&readfds);  
-     
-        //add master socket to set 
         FD_SET(master_socket, &readfds);  
         max_sd = master_socket;  
              
-        //add child sockets to set 
+        //emparentar los sockets a master
         for ( i = 0 ; i < max_clients ; i++)  
         {  
-            //socket descriptor 
-            sd = client_socket[i];  
-                 
+            sd = client_socket[i];     
             //if valid socket descriptor then add to read list 
             if(sd > 0)  
                 FD_SET( sd , &readfds);  
@@ -174,8 +166,6 @@ int main(int argc , char *argv[])
                 max_sd = sd;  
         }  
      
-        //wait for an activity on one of the sockets , timeout is NULL , 
-        //so wait indefinitely 
         activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);  
        
         if ((activity < 0) && (errno!=EINTR))  
@@ -183,8 +173,7 @@ int main(int argc , char *argv[])
             printf("select error");  
         }  
              
-        //If something happened on the master socket , 
-        //then its an incoming connection 
+        //Master socket recibe las conecciones nuevas
         if (FD_ISSET(master_socket, &readfds))  
         {  
             if ((new_socket = accept(master_socket, 
@@ -202,56 +191,58 @@ int main(int argc , char *argv[])
             j_request = json::parse(buffer);
             cout<<"deberia guardar este usuario a la lista : "<<j_request["body"][1]<<endl;
             
-            //send new connection greeting message 
+            //opcional
             if( send(new_socket, message, strlen(message), 0) != strlen(message) )  
             {  
                 perror("send");  
             }  
                  
-            puts("Welcome message sent successfully");  
+              
                  
-            //add new socket to array of sockets 
+             //Lo agrega hasta el final
             for (i = 0; i < max_clients; i++)  
             {  
-                //if position is empty 
+                
                 if( client_socket[i] == 0 )  
                 {  
                     client_socket[i] = new_socket;  
-                    printf("Adding to list of sockets as %d\n" , i);  
+                    //printf("Adding to list of sockets as %d\n" , i);  
                          
                     break;  
                 }  
             }  
         }  
              
-        //else its some IO operation on some other socket
+        //Sino se agrega un nuevo socket en modo listener es de modo cliente
         for (i = 0; i < max_clients; i++)  
         {  
             sd = client_socket[i];  
                  
             if (FD_ISSET( sd , &readfds))  
             {  
-                //Check if it was for closing , and also read the 
-                //incoming message 
+                //Manejador de 
                 if ((valread = read( sd , buffer, 1024)) == 0)  
                 {  
-                    //Somebody disconnected , get his details and print 
+                    //En caso de desconecciones 
                     getpeername(sd , (struct sockaddr*)&address , \
                         (socklen_t*)&addrlen);  
-                    printf("Host disconnected , ip %s , port %d \n" , 
+                    printf("Cliente se desconecto del chat, ip %s , port %d \n" , 
                           inet_ntoa(address.sin_addr) , ntohs(address.sin_port));  
                          
-                    //Close the socket and mark as 0 in list for reuse 
+                    //Libera el esacio del socket
                     close( sd );  
                     client_socket[i] = 0;  
                 }  
                      
-                //Echo back the message that came in 
                 else 
                 {  
-                    //set the string terminating NULL byte on the end 
+                    /
                     //of the data read 
                     cout<<"ESta es la solicitud que esta recibiendo" <<buffer <<endl;
+                    //Aqui se deben manejar las solicitudes.
+
+
+
                     buffer[valread] = '\0';  
                     send(sd , buffer , strlen(buffer) , 0 );  
                 }  
