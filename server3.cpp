3e7abe422729ef09a3d2ce46c52d8fceca32a3d4
coutino568 <40869460+coutino568@users.noreply.github.com>
2022-05-04 14:50:50 -0600

    Update server3.cpp

--- server3.cpp
+++ server3.cpp
@@ -151,7 +151,7 @@ int main(int argc , char *argv[])
                     printf("Message connect: %s", buffer);
                     json j_request;
                     j_request = json::parse(buffer);
-                    cout<<"deberia guardar este usuario a la lista : "<<j_request["body"][1]<<endl;
+                    std::cout<<"deberia guardar este usuario a la lista : "<<j_request["body"][1]<<std::endl;
                     printf("Adding to list of sockets as %d\n" , i);
                         
                     break;
