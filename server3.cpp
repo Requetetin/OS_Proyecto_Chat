34b6faa09009e76197103910881008e0583331a7
coutino568 <40869460+coutino568@users.noreply.github.com>
2022-05-04 16:39:52 -0600

    Update server3.cpp

--- server3.cpp
+++ server3.cpp
@@ -251,6 +251,7 @@ int main(int argc , char *argv[])
                 {  
                     //set the string terminating NULL byte on the end 
                     //of the data read 
+                    cout<<"ESta es la solicitud que esta recibiendo" <<buffer <<endl;
                     buffer[valread] = '\0';  
                     send(sd , buffer , strlen(buffer) , 0 );  
                 }  
