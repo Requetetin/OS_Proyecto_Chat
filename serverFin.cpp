c5999657b406d35c4abefe0ecc7f380e1f81c2b8
coutino568 <40869460+coutino568@users.noreply.github.com>
2022-05-04 18:56:14 -0600

    login update

--- serverFin.cpp
+++ serverFin.cpp
@@ -187,7 +187,7 @@ int main(int argc , char *argv[])
             j_request = json::parse(buffer);
             cout<<"deberia guardar este usuario a la lista : "<<j_request["body"][1]<<endl;
             char response[1024];
-            snprintf(response, sizeof(response), "{\"response\": \"INIT_CONEX\",\"code\": \"200\" }");
+            snprintf(response, sizeof(response), "{\"response\": \"INIT_CONEX\",\"code\": 200 }");
             cout<<  "ESTO RESPONDIO EL SERVIDOR: " <<response <<endl;
             send(new_socket, response, sizeof(response), 0);
             int ix = getNextClientIndex();
